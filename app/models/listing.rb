3be5768fd7f54c486be248abfe826e6702e9deeb
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-10-25 10:38:18 +1100

    added geocode to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,4 +7,12 @@ class Listing < ApplicationRecord
 	has_many :tags, through: :listing_tags
 
 	belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+
+	def full_street_address
+		[street,city,state,postcode].join(', ')
+	end
+
+	geocoded_by :full_street_address
+	after_validation :geocode
+	
 end
