6a662def81356d3f8e8c9febf6f1f0b3e94aec77
hsingfu <hsingfu.t@gmail.com>
2017-10-24 14:45:19 +1100

    populated db:seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+UserListing.destroy_all
+ListingTag.destroy_all
+Tag.destroy_all
+Listing.destroy_all
+User.destroy_all
+
+User.create([
+	{ name: "Peter", phone: "0422188199", email: "a@gmail.com"},
+	{ name: "Darryl", phone: "0422188199", email: "a@gmail.com"},	 
+	{ name: "Hsing", phone: "0422188199", email: "a@gmail.com"}
+])
+
+#password
+
+Listing.create(street: "45 William", city: "Melbourne", state: "Victoria", postcode: 3000, price: 10, description: "help me move", image: "furniture.png", creator: User.first)
+
+Tag.create([
+	{ name: "Transport" },
+	{ name: "Labour" },
+	{ name: "Supplies" }
+])
+
+UserListing.create([
+	{ user: User.all[1], listing: Listing.first },
+	{ user: User.all[2], listing: Listing.first }
+])
+
+ListingTag.create([
+	{ listing: Listing.first, tag: Tag.all[0] },
+	{ listing: Listing.first, tag: Tag.all[1] }
+])
+
+
+
