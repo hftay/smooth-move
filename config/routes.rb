6971c0442a775fd8c815b3fe0168eb07a1a27237
darryl <darrylcohen@iinet.net.au>
2017-10-24 16:17:08 +1100

    initial page stucture

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :listings
   resources :tags
   resources :users
+  root 'pages#login'
+  get '/options', to: 'pages#options'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
