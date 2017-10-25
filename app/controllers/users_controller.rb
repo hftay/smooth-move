4be54ca64d76ab6abdc504eab0dad21df5fbd9ff
Jess <jesesnz@gmail.com>
2017-10-25 12:11:54 +1100

    create user complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-  
-
-
   def create
-
+  	@user = User.new
+		@user.name = params[:name]
+		@user.email = params[:email]
+		@user.password = params[:password]
+		@user.save
     redirect_to '/login'
   end
-
-  
 end
