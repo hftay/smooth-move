33e2b469dd6161318aadc35e8dd316b108c88adb
hsingfu <hsingfu.t@gmail.com>
2017-10-26 21:00:46 +1100

    changed some images

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,13 +21,15 @@ User.create([
 ])
 
 Listing.create([
-	{title: "Van and one helper needed", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 30, description: "help me move my fridge and washer, short distance move!", remote_image_url: "https://s7d4.scene7.com/is/image/roomandboard/642103?$prodzoom0$&size=498,300&scl=1", creator: User.first, open: false, num_helpers_needed: 2},
-	{title: "Help move Bed and Mattress $50", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 49.99, description: "mattress is King size, will need strong helpers", remote_image_url: "https://s7d4.scene7.com/is/image/roomandboard/642103?$prodzoom0$&size=498,300&scl=1", creator: User.all[1], open: true, num_helpers_needed: 3},
-	{title: "Pickup truck needed for move", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 50, description: "truck needs to be huge!", remote_image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Ford_F-150_crew_cab_--_05-28-2011.jpg/1200px-Ford_F-150_crew_cab_--_05-28-2011.jpg", creator: User.all[2], open: true, num_helpers_needed: 9},
-	{title: "Need boxes and tape for move", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 15, description: "need many boxes!", remote_image_url: "https://images-na.ssl-images-amazon.com/images/I/41YMeClXekL._SL500_AC_SS350_.jpg", creator: User.all[3], open: true, num_helpers_needed: 2},
-	{title: "Desperately need help for moving inner CBD", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 30, description: "Would really appreciate some help!", remote_image_url: "https://i.pinimg.com/originals/01/e6/3b/01e63b3db69a0588553e681b820022cf.png", creator: User.all[4], open: true, num_helpers_needed: 3}, 
-	{title: "Help me help me", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 30, description: "Please x3", remote_image_url: "http://plantbasedrunner.com/wp-content/uploads/2013/01/moved.jpg", creator: User.all[0], open: true, num_helpers_needed: 5},
-	{title: "Help needed to move an incredible coffee table", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 10, description: "Pizza if you help", remote_image_url: "https://i.pinimg.com/736x/01/54/55/015455c2cb944d63b65781a03354e0fc--multifunctional-furniture-small-spaces-compact-furniture.jpg", creator: User.all[0], open: true, num_helpers_needed: 1}	
+	{title: "Help me move my fancy fridge", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 30, description: "Van and one helper needed, short distance move!", remote_image_url: "http://www.heartsremained.com/wp-content/uploads/2013/11/open-refrigerator.jpg", creator: User.all[0], open: true, num_helpers_needed: 2, moving_date: Time.now.to_date},
+	{title: "Help move Bed and Mattress", street: "1 Collins Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 49.99, description: "it's a king mattress so will need some serious muscles", remote_image_url: "https://s7d4.scene7.com/is/image/roomandboard/642103?$prodzoom0$&size=498,300&scl=1", creator: User.all[1], open: true, num_helpers_needed: 3, moving_date: Time.now.to_date},
+	{title: "Borrow vehicle for move", street: "130 Dudley Street", city: "Melbourne", state: "Victoria", postcode: 3003, price: 50, description: "need vehicle for 1 hour use to move stuff", remote_image_url: "http://www.cumminsforum.com/forum/attachments/4th-gen-non-powertrain/36195d1311561753-let-me-see-black-trucks-img_1446.jpg", creator: User.all[2], open: true, num_helpers_needed: 1, moving_date: Time.now.to_date},
+	{title: "Supplies needed for relocation", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 10, description: "need many boxes, ideally evenly sized, and high quality ones!", remote_image_url: "http://boxbros.com/wp-content/uploads/2016/03/moving-happy-boxes.jpg", creator: User.all[3], open: true, num_helpers_needed: 1, moving_date: Time.now.to_date},
+	{title: "Desperately need help with moving inner CBD this weekend", street: "1 Exhibition Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 10, description: "Will offer a can of beer", remote_image_url: "https://s.hswstatic.com/gif/moving-checklist-1.jpg", creator: User.all[4], open: true, num_helpers_needed: 3, moving_date: Time.now.to_date},
+	{title: "Help needed to move couch, will pay top dollars", street: "23 Therry Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 150, description: "please contact if interested", remote_image_url: "https://usercontent1.hubstatic.com/6780598_f1024.jpg", creator: User.all[0], open: true, num_helpers_needed: 5, moving_date: Time.now.to_date},
+	{title: "Help needed to move an incredible coffee table", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 35, description: "Pizza if you help", remote_image_url: "https://i.pinimg.com/736x/01/54/55/015455c2cb944d63b65781a03354e0fc--multifunctional-furniture-small-spaces-compact-furniture.jpg", creator: User.all[0], open: true, num_helpers_needed: 1, moving_date: Time.now.to_date},
+	{title: "This washing machine is not going to move itself", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 25, description: "Help needed to move", remote_image_url: "http://ksassets.timeincuk.net/wp/uploads/sites/54/2017/01/Best-Washing-Machines.jpg", creator: User.all[0], open: true, num_helpers_needed: 1, moving_date: Time.now.to_date}	,
+	{title: "Bought this on gumtree, need help moving", street: "45 William Street", city: "Melbourne", state: "Victoria", postcode: 3000, price: 80, description: "it's heavy", remote_image_url: "https://www.diamondbackbilliards.com/img/Olhausen/Olhausen-Billiards-Rococo-Pool-Tables-L.jpg", creator: User.all[0], open: true, num_helpers_needed: 1, moving_date: Time.now.to_date}			
 	])
 
 Tag.create([
@@ -37,19 +39,32 @@ Tag.create([
 ])
 
 UserListing.create([
-	{ user: User.all[1], listing: Listing.first },
-	{ user: User.all[2], listing: Listing.first }
+	{ user: User.all[1], listing: Listing.all[0] },
+	{ user: User.all[2], listing: Listing.all[1] },
+	{ user: User.all[0], listing: Listing.all[1] },
+	{ user: User.all[0], listing: Listing.all[5] }		
 ])
 
 ListingTag.create([
-	{ listing: Listing.first, tag: Tag.all[0] },
-	{ listing: Listing.first, tag: Tag.all[1] },
+	{ listing: Listing.all[0], tag: Tag.all[0] },
+	{ listing: Listing.all[0], tag: Tag.all[1] },
 	{ listing: Listing.all[1], tag: Tag.all[0] },
 	{ listing: Listing.all[1], tag: Tag.all[1] },	
-	{ listing: Listing.all[2], tag: Tag.all[2] },
+	{ listing: Listing.all[2], tag: Tag.all[0] },
 	{ listing: Listing.all[3], tag: Tag.all[2] },
-	{ listing: Listing.all[4], tag: Tag.all[1] },	
+	{ listing: Listing.all[4], tag: Tag.all[0] },	
+	{ listing: Listing.all[4], tag: Tag.all[1] },
+	{ listing: Listing.all[4], tag: Tag.all[2] },	
+
 	{ listing: Listing.all[5], tag: Tag.all[0] },
 	{ listing: Listing.all[5], tag: Tag.all[1] },	
-	{ listing: Listing.all[5], tag: Tag.all[2] }	
+	{ listing: Listing.all[5], tag: Tag.all[2] },	
+
+	{ listing: Listing.all[6], tag: Tag.all[1] },	
+
+	{ listing: Listing.all[7], tag: Tag.all[1] },	
+
+	{ listing: Listing.all[8], tag: Tag.all[0] },
+	{ listing: Listing.all[8], tag: Tag.all[1] }
+
 ])
