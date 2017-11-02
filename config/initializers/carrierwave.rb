f32d79d9d07eb5953e48feb104f3763e76f36a52
hsingfu <hsingfu.t@gmail.com>
2017-11-02 21:35:42 +1100

    deleted the gazillion images in public folder" "

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,17 +6,9 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],                        # required
     region:                'ap-southeast-2',                  # optional, defaults to 'us-east-1'
     host:                  's3-ap-southeast-2.amazonaws.com',             # optional, defaults to nil
-    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = 'wdi-smooth-move'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
+  # config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
-end
-
-# class ImageUploader < CarrierWave::Uploader::Base
-# end
-
-class Post < ActiveRecord::Base
-  mount_uploader :image, ImageUploader
- 
 end
